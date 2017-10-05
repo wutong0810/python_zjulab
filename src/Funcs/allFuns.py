2c66bcf2f0aa6f5c39b281ae5c9d00a059852a47
wutongshu <gongyue@zju.edu.cn>
2017-10-05 12:08:27 +0800

    10-05图书馆

--- src/Funcs/allFuns.py
+++ src/Funcs/allFuns.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 11 13:46:30 2017
+
+@author: wutongshu
+"""
 import pandas as pd
 import numpy as np
 
@@ -9,4 +15,17 @@ def loadData(path0):
     return data
 
 
+
+# data第1列MONTH,第2列DAYS，第3列SJ，第4列N
+def countData(data):
+    dataCount=pd.pivot_table(data,index='SJ',columns=['MONTH','DAYS'],values='N')
+    dataCount.columns = [col for col in dataCount.columns]
+    dataCount=dataCount.reset_index()
+    return dataCount
+
+
+
+if __name__=='__main__':
+    data=loadData(path0=r'C:\Users\wutongshu\Desktop\雨天数据\10-03count_zhonghua.csv')
+    dataCount=countData(data)
 
